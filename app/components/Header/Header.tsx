2ffa53b4f6f45ded349240372b0e36c3a23f83bc
sajidali1980 <sajidmumtaz90@yahoo.com>
2024-11-10 12:56:04 +0500

    Initial commit

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+const Header=()=>{
+return (
+    <nav className="h-full w-full bg-primary py-2 px-4 sticky top-[0px] z-50 bg-[#A29875]">
+<div className="text-headings font-algerian text-4xl text-center lg:text-start text=[#ffff]">Manzzari</div></nav>
+)
+}
+export default Header
